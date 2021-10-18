218032f1181b8970c212be353a392385e11a071b
westshae <shaewest02@gmail.com>
2021-10-18 22:34:52 +1300

    Commented/refactored Weathergrid.tsx

--- src/components/weather/WeatherGrid.tsx
+++ src/components/weather/WeatherGrid.tsx
@@ -10,20 +10,19 @@ const StyledDiv = styled.div`
   gap:0.5rem;
 `
 
-
 const WeatherGrid = (props:any) =>{
+  //Stores all data received from weather JSON
   const [current, setCurrent] = useState(null);
   const [forecast, setForecast] = useState(null);
   const [astrology, setAstrology] = useState(null);
 
-  useEffect(()=>{
+  useEffect(()=>{//If props updates, update local variables
     if(props.data != undefined){
       setCurrent(props.data.current);
       setForecast(props.data.forecast);
       setAstrology(props.data.astrology);
     }
   }, [props]);
-  
 
  return(
    <StyledDiv>
